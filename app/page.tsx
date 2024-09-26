b8a769fdade5a63316199b25983fcaa5730feca6
abel <abelaatkelet@gmail.com>
2024-09-26 11:53:44 +0300

    change

--- app/page.tsx
+++ app/page.tsx
@@ -21,10 +21,10 @@ export default async function Home() {
             </span>
 
             <h1 className="mt-8 text-3xl font-extrabold tracking-tight lg:text-6xl">
-              Create Notes with ease
+            Create and manage notes effortlessly
             </h1>
             <p className="max-w-xl mx-auto mt-8 text-base lg:text-xl text-secondary-foreground">
-            Effortlessly capture, organize, and access your notes anywhere, anytime with Note Cloud. Secure,
+            Effortlessly capture and access your notes anytime with Note Cloud. Secure,
            for individuals and teams.
             </p>
           </div>
